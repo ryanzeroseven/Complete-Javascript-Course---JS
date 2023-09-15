0c9ccd0424f75c5205bb10ec8d7c5c50a15f6400
Irsjaad Mahabier <irmahabier@gmail.com>
2023-09-15 13:14:20 +0200

    08-Behind-the-Scenes, video 93, halfway in

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+function calcAge(birthyear) {
+  const age = 2037 - birthyear;
+  // console.log(firstName);
+
+  function printAge() {
+    const output = `${firstName}, you are ${age} and born in ${birthyear}`;
+    console.log(output);
+  }
+  printAge();
+
+  return age;
+}
+
+const firstName = 'Irsjaad';
+calcAge(1991);
+// console.log(age);
+// printAge;
