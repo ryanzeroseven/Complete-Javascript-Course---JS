d433bcd8ef79b01838bf0fa00ed7ed3d65843554
Irsjaad Mahabier <irmahabier@gmail.com>
2023-09-22 14:45:33 +0200

    08-Behind-the-Scenes - video 93, finished video lecture

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -2,10 +2,29 @@
 
 function calcAge(birthyear) {
   const age = 2037 - birthyear;
-  // console.log(firstName);
 
   function printAge() {
-    const output = `${firstName}, you are ${age} and born in ${birthyear}`;
+    let output = `${firstName}, you are ${age}, born in ${birthyear}`;
+    console.log(output);
+
+    if (birthyear >= 1981 && birthyear <= 1996) {
+      var millenial = true;
+      // Creating NEW variable with same name as outer scope variable
+      const firstName = 'Steven'; //Because the const firstName is within the scope, JS wont look outside to the global variables
+
+      // Reassigning outer scope's variable
+      output = 'NEW OUTPUT!';
+
+      const str = `oh, and you're a millenial, ${firstName}`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+    }
+    // console.log(str);
+    console.log(millenial);
+    // console.log(add(2, 3));
     console.log(output);
   }
   printAge();
@@ -13,7 +32,9 @@ function calcAge(birthyear) {
   return age;
 }
 
+//global variable therefore useable in the function above
 const firstName = 'Irsjaad';
+
 calcAge(1991);
 // console.log(age);
-// printAge;
+// printAge();
