5a51ded16284205b37b95c65dbac60873d283505
Irsjaad Mahabier <irmahabier@gmail.com>
2023-10-21 14:23:18 +0200

    09-Data-Structures-Operators - video 103, array destructuring

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -12,6 +12,10 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
   openingHours: {
     thu: {
       open: 12,
@@ -27,3 +31,43 @@ const restaurant = {
     },
   },
 };
+
+// VIDEO 103
+// DESTRUCTURING ARRAYS
+
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+const [x, y, z] = arr;
+console.log(x, y, z); // 2, 3, 4
+console.log(arr);
+
+let [main, , secondary] = restaurant.categories;
+console.log(main, secondary); // Italian, 'skipped', Vegetarian
+
+//SWITCHING VARIABLES
+
+// solution to switch variables wihtout destructuring
+// const temp = main;
+// main = secondary;
+// secondary = temp;
+// console.log(main, secondary);
+
+[main, secondary] = [secondary, main];
+console.log(main, secondary);
+
+// Receive 2 return values from a function
+const [starter, maincourse] = restaurant.order(2, 0);
+console.log(starter, maincourse);
+
+// Nested destructuring
+const nested = [2, 4, [5, 6]];
+// const [i, , j] = nested;
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
+
+// Default values
+const [p = 1, q = 1, r = 1] = [8, 9];
+console.log(p, q, r); // 9, 8, 1
