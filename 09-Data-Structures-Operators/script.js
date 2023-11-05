0058424eb6cbeb7ea83654b73afac6c26d7ded1e
Irsjaad Mahabier <irmahabier@gmail.com>
2023-11-05 14:54:53 +0100

    test commit

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -381,3 +381,5 @@ console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exist');
 const users = [{ name: 'Irsjaad', email: 'Hello@irsjaad.io' }];
 
 console.log(users[0]?.name ?? 'User array empty');
+
+console.log('some test');
