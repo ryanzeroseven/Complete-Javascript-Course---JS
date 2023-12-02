1521845741fc202956ee0cdf81acdb8336ee4a70
Irsjaad Mahabier <irmahabier@gmail.com>
2023-12-02 18:59:11 +0100

    10-Functions - video 138, closure examples

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -307,3 +307,48 @@ booker();
 // A closure is like a backpack that a function carries around wherever it goes. This backpack has all the variables that were present in the environment where the function was created.
 
 console.dir(booker);
+
+// VIDEO 138
+// More Closure Examples
+
+// Example 1
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f(); // 46
+console.dir(f);
+
+// Re-assigned f function
+h();
+f(); // 1554
+
+console.dir(f);
+
+// Example 2
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+
+const perGroup = 1000;
+boardPassengers(180, 3);
