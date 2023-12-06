60ea8f1860f234720df4a581be9f534589dbb1d6
Irsjaad Mahabier <irmahabier@gmail.com>
2023-12-06 18:45:40 +0100

    11-Working-With-Arrays - video 152, filter method

--- 11-Arrays-Bankist/lectures.js
+++ 11-Arrays-Bankist/lectures.js
@@ -151,3 +151,22 @@ const movementsDescriptions = movements.map(
     )}`
 );
 console.log(movementsDescriptions);
+
+// VIDEO 152;
+// The Filter Method
+
+const deposits = movements.filter((mov) => mov > 0);
+console.log(movements);
+console.log(deposits);
+
+// For loop
+const depositsFor = [];
+for (const mov of movements) {
+  if (mov > 0) {
+    depositsFor.push(mov);
+  }
+}
+console.log(depositsFor);
+
+const withdrawals = movements.filter((mov) => mov < 0);
+console.log(withdrawals);
