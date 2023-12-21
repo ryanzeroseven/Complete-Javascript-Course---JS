46976c9c7f1013ed7ae2ab85791f34dffce5826d
Irsjaad Mahabier <irmahabier@gmail.com>
2023-12-21 10:42:26 +0100

    12-Numbers-Dates-Intl-Timers - video 171, rouding numbers with Math functions

--- 12-Numbers-Dates-Timers-Bankist/lectures.js
+++ 12-Numbers-Dates-Timers-Bankist/lectures.js
@@ -36,3 +36,54 @@ console.log(Number.isFinite(23 / 0)); // false
 console.log(Number.isInteger(23)); // true
 console.log(Number.isInteger(23.0)); // true
 console.log(Number.isInteger(23 / 0)); // false
+
+// VIDEO 170
+// Math and Rounding
+
+//* Square root
+console.log(Math.sqrt(25)); // 5
+console.log(25 ** (1 / 2)); // 5
+console.log(8 ** (1 / 3)); // 2
+
+//* Max & Min
+console.log(Math.max(5, 18, 23, 11, 2)); // 23
+console.log(Math.max(5, 18, '23', 11, 2)); // 23
+console.log(Math.max(5, 18, '23px', 11, 2)); // NaN
+
+console.log(Math.min(5, 18, '23', 11, 2)); // 2
+
+//* Constants
+console.log(Math.PI * Number.parseFloat('10px') ** 2);
+
+console.log(Math.trunc(Math.random() * 6) + 1);
+
+//* Random number generator
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min) + 1) + min;
+// console.log(randomInt(10, 20));
+
+//* Rounding integers
+
+//* Round
+console.log(Math.round('23.3')); // 23
+console.log(Math.round(23.7)); // 24
+
+//* Ceil, nearest upper integer
+console.log(Math.ceil(23.3)); // 24
+console.log(Math.ceil('23.7')); // 24
+
+//* Floor nearest lower integer
+console.log(Math.floor(23.3)); // 23
+console.log(Math.floor('23.7')); // 23
+
+//? trunc and floor work the same for positive numbers, but for negative numbers floor is more accurate.
+console.log(Math.trunc(23.3)); // 23
+
+console.log(Math.trunc(-23.3)); // -23
+console.log(Math.floor(-23.3)); // -24
+
+// Decimals (Floating point numbers)
+console.log((2.7).toFixed(0)); // 3 - string
+console.log((2.7).toFixed(3)); // 2.700 - string
+console.log((2.345).toFixed(2)); // 2.35 - string
+console.log(+(2.345).toFixed(2)); // 2.35 - number
--- 12-Numbers-Dates-Timers-Bankist/script.js
+++ 12-Numbers-Dates-Timers-Bankist/script.js
@@ -100,7 +100,7 @@ const displayMovements = function (movements, sort = false) {
       <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-      <div class="movements__value">€ ${mov}</div>
+      <div class="movements__value">€ ${mov.toFixed(2)}</div>
     </div>
     `;
 
@@ -143,7 +143,7 @@ const updateUI = function (acc) {
 const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce((arr, mov) => arr + mov, 0);
 
-  labelBalance.textContent = `${acc.balance} EUR`;
+  labelBalance.textContent = `${acc.balance.toFixed(2)} EUR`;
 };
 //? calcDisplayBalance(account1.movements);
 
@@ -154,12 +154,12 @@ const calcDisplaySummary = function (acc) {
   const incomes = acc.movements
     .filter((mov) => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = `€${incomes} `;
+  labelSumIn.textContent = `€${incomes.toFixed(2)} `;
 
   const out = acc.movements
     .filter((mov) => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumOut.textContent = `€${Math.abs(out)} `;
+  labelSumOut.textContent = `€${Math.abs(out).toFixed(2)} `;
 
   const interest = acc.movements
     .filter((mov) => mov > 0)
@@ -169,7 +169,7 @@ const calcDisplaySummary = function (acc) {
       return int >= 1;
     })
     .reduce((acc, int) => acc + int, 0);
-  labelSumInterest.textContent = `€${interest} `;
+  labelSumInterest.textContent = `€${interest.toFixed(2)} `;
 };
 //? calcDisplaySummary(account1.movements);
 
@@ -288,7 +288,7 @@ btnClose.addEventListener('click', function (e) {
 
 btnLoan.addEventListener('click', function (e) {
   e.preventDefault();
-  const amount = +inputLoanAmount.value;
+  const amount = Math.floor(inputLoanAmount.value);
 
   if (
     amount > 0 &&
