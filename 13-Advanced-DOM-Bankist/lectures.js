2f54f839b51b45355249211aec68b180762323da
Irsjaad Mahabier <irmahabier@gmail.com>
2023-12-31 21:58:30 +0100

    13-Advanced-DOM-Events - video 186 & 187, element manipulation and styles

---
+++ 13-Advanced-DOM-Bankist/lectures.js
@@ -0,0 +1,80 @@
+// VIDEO 186
+// Selecting, Creating and Deleting Elements
+
+//* Selecting
+
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+document.querySelector('.header');
+const allSections = document.querySelectorAll('.section');
+console.log(allSections);
+
+document.getElementById('section--1');
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);
+
+console.log(document.getElementsByClassName('btn'));
+
+//* Creating and inserting elements
+// .insertAdjacentHTML()
+
+const div = document.createElement('div');
+div.classList.add('cookie-message');
+// message.textContent = 'We use cookies for improved functionality and analytics.'
+div.innerHTML = //? replace the complete inner HTML
+  'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+
+// header.prepend(message); //? insert as first child
+// header.append(message); //? insert as last child
+// header.append(message.cloneNode(true)); //? clone child
+
+// header.before(message); //? before the element (sibling)
+// header.after(message); //? after the element (sibling)
+
+// VIDEO 187
+// StyleSheet, Attributes and Classes
+
+//* Styles
+div.style.backgroundColor = '#37383d';
+console.log(div.style.backgroundColor); // rgb(55, 56, 61)
+console.log(div.style.height); //? '', because this only works for inline styles we have set before.
+
+console.log(getComputedStyle(message).color); // rgb(187, 187, 187)
+console.log(getComputedStyle(message).height); // 50px
+
+document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+//* Attributes
+const logo = document.querySelector('.nav__logo');
+
+//? Standard attributes
+console.log(logo.alt);
+console.log(logo.src); //? Absolute path:http://127.0.0.1:5501/img/logo.png
+console.log(logo.getAttribute('src')); //? Relative path: img/logo.png
+console.log(logo.className);
+
+logo.alt = 'Beautiful minimalist logo';
+
+//? Non-standard attributes
+console.log(logo.designer); // undefined
+console.log(logo.getAttribute('designer')); // Jonas Schmedtman
+console.log(logo.setAttribute('company', 'Bankist'));
+
+//? Absolute and relative paths of the href
+const navBtn = document.querySelector('.nav__link--btn');
+console.log(navBtn.href);
+console.log(navBtn.getAttribute('href'));
+
+//* Data attributes
+console.log(logo.dataset.versionNumber);
+
+//* Classes
+logo.classList.add('a');
+logo.classList.remove('a');
+logo.classList.toggle('a');
+logo.classList.contains('a');
+
+//! Don't use, it will overwrite all other classes and allows only 1 class.
+// logo.className = 'irsjaad';
--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -8,7 +8,8 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const openModal = function (e) {
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -18,8 +19,10 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach((btn) => btn.addEventListener(' click', openModal));
+
+// for (let i = 0; i < btnsOpenModal.length; i++)
+//   btnsOpenModal[i].addEventListener('click', openModal);
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -29,3 +32,33 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+// VIDEO 186
+
+const header = document.querySelector('.header');
+
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+// message.textContent = 'We use cookies for improved functionality and analytics.'
+message.innerHTML =
+  'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+
+header.append(message);
+
+//* Delete elements
+
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', function () {
+    message.remove();
+  });
+
+// VIDEO 187
+// StyleSheet, Attributes and Classes
+
+//* Styles
+message.style.backgroundColor = '#37383d';
+message.style.width = '98vw';
+
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 40 + 'px';
